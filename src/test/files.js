1486b5e39e1dbc89a4b03ea85b0a2d4f0d88af02
Nahuel Jaume <nahueljaume@gmail.com>
2023-03-26 19:13:59 -0300

    test

---
+++ src/test/files.js
@@ -0,0 +1,30 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = "test";
+
+//Require the dev-dependencies
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let should = chai.should();
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("Files", () => {
+  /*
+   * Test the /GET files route
+   */
+  describe("/GET files", () => {
+    it("it should GET all the files", (done) => {
+      chai
+        .request("http://localhost:3000")
+        .get("/files/data")
+        .end((err, res) => {
+          console.log("body", res.body);
+          res.should.have.status(200);
+          res.body.should.be.a("array");
+          res.body.every((i) => expect(i).to.have.all.keys("file", "lines"));
+          done();
+        });
+    });
+  });
+});
